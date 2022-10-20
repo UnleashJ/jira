a3c89465dc1b92323b094d9627b62e2b197bbf30
UnleashJ <1131223002@qq.com>
2022-10-20 22:18:39 +0800

    fix: useMount and useDebounce

--- src/screens/projec-list/index.jsx
+++ src/screens/projec-list/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { List } from './list'
 import { SearchPanel } from './search-panel'
 import qs from 'qs'
-import { cleanObject } from 'utils/index'
+import { cleanObject, useMount, useDebounce } from 'utils/index'
 
 const apiURL = process.env.REACT_APP_API_URL
 
@@ -15,21 +15,23 @@ export const ProjectListScreen = () => {
   }) // 请求参数
   const [list, setList] = useState([]) // 工程列表数据
 
+  const debouncedParam = useDebounce(param)
+
   useEffect(() => {
-    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
+    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async res => {
       if(res.ok) {
         setList(await res.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiURL}/users`).then(async res => {
       if(res.ok) {
         setUsers(await res.json())
       }
     })
-  }, [])
+  })
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -9,4 +11,23 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (cb) => {
+  useEffect(cb, []);
+};
+
+// 防抖，包装一个参数value，返回一个新参数debouncedParam。
+// 连续改变value，间隔delay毫秒及以上，才真的改变value
+export const useDebounce = (value, delay = 500) => {
+  const [param, setParam] = useState(value);
+
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      setParam(value);
+    }, delay);
+    return () => clearTimeout(timerId); // 利用useEffect清除副作用的特点，在下一次执行前清除定时器
+  }, [value, delay]);
+
+  return param;
 };
